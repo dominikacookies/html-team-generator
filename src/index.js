6e13ac45d6f4670e2027de8736a34dc3100e1a4a
Dominika <domi_nika@hotmail.co.uk>
2021-05-08 22:16:39 +0100

    added pseudo code for index.js and new files for employee creation

---
+++ src/index.js
@@ -0,0 +1,8 @@
+init = () => {
+  // const manager = generateManager()
+  // const employees = generateDirectReports(manager)
+  // const html = generateHtml(employees)
+  // createFile(html)
+}
+
+init ()
