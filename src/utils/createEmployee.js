f1a1eea21367b5cceecd788e70bc999429b9bdc9
Dominika <domi_nika@hotmail.co.uk>
2021-05-09 13:04:58 +0100

    gets employee name and email

--- src/utils/createEmployee.js
+++ src/utils/createEmployee.js
@@ -1,3 +1,5 @@
+const inquirer = require("inquirer");
+
 class Employee {
   constructor(name, email, id, role) {
     this.name = name || "TBC",
@@ -5,17 +7,37 @@ class Employee {
     this.id = id || "TBC"
     this.role = role || "Employee"
   }
-  getName () {
-    console.log("name")
+  async getName (role)  {
+    const question = 
+    {
+      type: "input",
+      message: `what is the ${role}'s name?`,
+      name: "nameInput",
+    }
+
+    const name = await inquirer.prompt(question)
+    return name.nameInput
   }
-  getEmail () {
-    console.log("email is here")
-    return "email X"
+
+  async getEmail (name) {
+    const question = 
+    {
+      type: "input",
+      message: `What is your ${name}'s address?`,
+      name: "emailInput",
+      validate: function (email) {
+        return /^(([^<>()\[\]\.,;:\s@\"]+(\.[^<>()\[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()\.,;\s@\"]+\.{0,1})+([^<>()\.,;:\s@\"]{2,}|[\d\.]+))$/.test(
+          email
+        );
+      },
+    }
+
+    const email = await inquirer.prompt(question)
+    return email.emailInput
   }
   getId () {
     console.log("id")
   }
-
 }
 
 module.exports = Employee
--- src/utils/createManager.js
+++ src/utils/createManager.js
@@ -1,10 +1,14 @@
+const inquirer = require("inquirer");
+
 const Employee = require("./createEmployee")
 
-const createManager = () => {
-  const newEmployee = new Employee()
-  console.log(newEmployee)
-  const emailHere = newEmployee.getEmail()
-  console.log(emailHere)
+const createManager = async () => {
+  const managerBasicInformation = new Employee()
+  console.log(managerBasicInformation)
+  const employeeName = await managerBasicInformation.getName("manager")
+  console.log(employeeName)
+  const employeeEmail = await managerBasicInformation.getEmail(employeeName)
+  console.log(employeeEmail)
 }
 
 module.exports = createManager; 
